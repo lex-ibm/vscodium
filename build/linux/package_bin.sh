f2ea651d64d4a724f5a3eef6c73d8272ba1c3d21
Alex Garcia <lex@ibm.com>
2026-07-06 13:19:04 -0500

    feat(ppc64le): Use GCC-10.5.0 for REH and BIN

--- build/linux/package_bin.sh
+++ build/linux/package_bin.sh
@@ -28,8 +28,7 @@ if [[ "${VSCODE_ARCH}" == "arm64" ]]; then
   # export USE_GNUPP2A=1
 elif [[ "${VSCODE_ARCH}" == "ppc64le" ]]; then
   export VSCODE_SYSROOT_REPOSITORY='VSCodium/vscode-linux-build-agent'
-  export VSCODE_SYSROOT_VERSION='20240129-253798'
-  export VSCODE_SYSROOT_PREFIX="-glibc-2.28"
+  export VSCODE_SYSROOT_VERSION='v20260706'
   export ELECTRON_SKIP_BINARY_DOWNLOAD=1
   export PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD=1
   export VSCODE_ELECTRON_REPOSITORY='lex-ibm/electron-ppc64le-build-scripts'
